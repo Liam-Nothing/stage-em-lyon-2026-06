a32a9783c7e1b2b8a164796d31fc6600f026e897
cheimacherni-jpg <chema.cherni@gmail.com>
2026-09-16 13:59:22 +0200

    create (js- css) : fonction livre.js + style css

--- js/livre.js
+++ js/livre.js
@@ -1,19 +1,36 @@
 
 
+async function initLivre() {
+    let parametres = new URLSearchParams(window.location.search);
+    let id = parametres.get("id");
+    
+    console.log("id lu dans l'URL :", id);
+    
+    if (id === null) {
+    afficherErreur();
+    return;
+  }
+   let livres = await chargerLivres();
 
-let parametres = new URLSearchParams(window.location.search);
-let id =parametres.get("id");
-console.log("id lu dans l'URL :", id);
+   let livre = trouverLivre(livres, id);
 
-////fonction afficher l'erreur quand livre introuvable////
-
-function afficherErreur() {
-    document.querySelector("div-row container fil-activite").innerHTML =
-    `<p>Livre introuvable</p>
-    <a href="library.html">Retour à la library</a>`;
-
+    if (livre === undefined) {
+    afficherErreur();
+    return;
+  }
+afficherLivre(livre);
 }
 
 function trouverLivre(livres, id) {
-    return livres.find(livre => livre.id ===id);
+  return livres.find(livre => livre.id === id);
 }
+
+function afficherErreur() {
+  document.querySelector(".div-row.container.fil-activite").innerHTML = `
+    <p>Livre introuvable.</p>
+    <a href="bibliotheque.html">Retour à la bibliothèque</a>
+  `;
+}
+
+
+//livre.html?id=0003
