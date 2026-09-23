ad3a2f60a373de5a3b39ef307c793637cc9bd84c
cheimacherni-jpg <chema.cherni@gmail.com>
2026-09-23 13:24:58 +0200

    create(index.html): div pour fil d'actu

--- js/amis.js
+++ js/amis.js
@@ -40,6 +40,51 @@ function retirerAmie(id) {
 }
 
 
-////FONCTION AFFICHER CARTE AMIE ///
+////FONCTION CONSTRUIRE FIL ///
+
+async function construireFil() {
+    // Liste des amies (issue de js/amis.js)
+    const utilisatrices = await listerUtilisatrices();
+    const amies = utilisatrices.filter(u => u.estAmie);
+    const idsAmies = amies.map(a => a.id);
+
+    // Sources : JSON + localStorage
+    const avisJSON = await chargerAvis();
+    const avisLocalJSON = localStorage.getItem("avisLocaux");
+    const avisLocaux = avisLocalJSON ? JSON.parse(avisLocalJSON) : [];
+
+    const toutesLesEntrees = [...avisJSON, ...avisLocaux];
+
+    const evenementsNotes = [];
+    const evenementsAvis = [];
+
+    // Passe 1 : les notes chiffrées
+    for (const entree of toutesLesEntrees) {
+        if (idsAmies.includes(entree.idUtilisateur) && entree.note != null) {
+            evenementsNotes.push({
+                type: "note",
+                idAmie: entree.idUtilisateur,
+                idLivre: entree.idLivre,
+                date: entree.datePublicationCommentaire
+            });
+        }
+    }
+
+    // Passe 2 : les avis écrits
+    for (const entree of toutesLesEntrees) {
+        if (idsAmies.includes(entree.idUtilisateur) && entree.commentaire) {
+            evenementsAvis.push({
+                type: "avis",
+                idAmie: entree.idUtilisateur,
+                idLivre: entree.idLivre,
+                date: entree.datePublicationCommentaire
+            });
+        }
+    }
+
+    // Fusion finale
+    return [...evenementsNotes, ...evenementsAvis];
+}
+
 
 
